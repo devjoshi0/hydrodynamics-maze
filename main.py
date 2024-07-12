54f3cd84b6555a2ac385eeec3084542b1e9399ba
Dev Joshi <GalaxiDev0@gmail.com>
2024-07-12 14:14:06 -0400

    fixed pygame not running on my operating system linux

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
+import random
 from maze import *
+from wall import *
 import pygame
 
+WHITE = (255,255,255)
+GREY = (20,20,20)
+BLACK = (0,0,0)
+PURPLE = (128,0,128)
+RED = (255,0,0)
+BLUE = (0,0,255)
 
-WINSIZE = [640, 480]
+size = 701,701
+
+
+    
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(WINSIZE)
-    screen_info = pygame.display.Info()
+    screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Maze Game')
-    screen.fill((0,0,0))
+    screen.fill((255,255,255))
 
     clock = pygame.time.Clock()
     
